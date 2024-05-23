9bc5dc44e259700113c235abd025ed9df61899e0
Joel Lawi <abecedegluu@gmail.com>
2024-05-23 18:10:20 -0500

    Update de urls api

--- BackEnd/api/urls.py
+++ BackEnd/api/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path, include
 from .view.ProductView import ProductViewSet
+from .view.CategoryView import CategoryViewSet
+from .view.BrandView import BrandViewSet
 
 urlpatterns = [
     # path('auth/register',views.UserRegisterView.as_view(),name="user_register"),#Registrar un usuario
     #path('api/product', ProductViewSet.as_view({'get': 'list', 'post': 'create'}), name="product")
-    path('', ProductViewSet.as_view({'get': 'list', 'post': 'create'}), name="product-list"),  # Esta es la ruta para la URL raíz
+    path('product', ProductViewSet.as_view({'get': 'list', 'post': 'create'}), name="product-list"),  
     path('product/<int:pk>', ProductViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name="product"),
+
+    path('category/', CategoryViewSet.as_view({'get': 'list', 'post': 'create'}), name='category-list'),
+    path('category/<int:pk>', CategoryViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),name='category'),
+
+    path('brand/', BrandViewSet.as_view({'get': 'list', 'post': 'create'}), name='brand-list'),
+    path('brand/<int:pk>', BrandViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),name='brand'),
 
 ]
