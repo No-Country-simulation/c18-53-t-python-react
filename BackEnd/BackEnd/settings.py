0f89ee8ab32d7f82e36bdbf6f8e35da9ab2b5999
Joel Lawi <abecedegluu@gmail.com>
2024-05-25 17:42:02 -0500

    Config de conexion con postgreSQL

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -85,6 +85,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'beatica db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
