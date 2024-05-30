6a733202ac848fdce4df0926e50de2c2f9e43c88
Joel Lawi <abecedegluu@gmail.com>
2024-05-30 18:19:59 -0500

    Archivos de Payment

---
+++ BackEnd/api/Payment/config.py
@@ -0,0 +1,18 @@
+import mercadopago
+
+sdk = mercadopago.SDK("YOUR_ACCESS_TOKEN")
+
+payment_data = {
+    "transaction_amount": 100,
+    "token": "CARD_TOKEN",
+    "description": "Payment description",
+    "payment_method_id": 'visa',
+    "installments": 1,
+    "payer": {
+        "email": 'test_user_123456@testuser.com'
+    }
+}
+result = sdk.payment().create(payment_data)
+payment = result["response"]
+
+print(payment)
